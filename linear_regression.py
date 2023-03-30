c9c77f73a6c3d8dd2bc7a2309f1cb55e990ec340
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-03-30 11:34:29 +0300

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,32 +1,87 @@
+# An example classification --> Breast Cancer Model
+import pandas as pd
+import tensorflow as tf
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_squared_error as mse
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import linear_model as lm
-from sklearn.model_selection import train_test_split as split
+import seaborn as sns
 
-# 1. Generate a set of 500 evenly spaced values between -10 and 10.
-x = np.linspace(-10, 10, 500)
 
-# 2. Define the function to be learned by the neural network.
-y = 5 * x**3 - 2 * x**2 -1 * x + 5
+def plot_confusion_matrix(cm):
+    """
+    This function takes in true labels and predicted labels as NumPy arrays and plots the corresponding confusion matrix using Seaborn.
 
-# 3. Split the data into training and testing sets, with a test set size of 20%.
-# The input data for the neural network is the transpose of the array [x, x^2, x^3, x^4].
-# The output data is the array y.
-x_train, x_test, y_train, y_test = split(np.asarray([x, x**2, x**3, x**4]).T, y, test_size=0.2)
+    Parameters:
+        y_true (numpy.ndarray): True labels represented as a NumPy array.
+        y_pred (numpy.ndarray): Predicted labels represented as a NumPy array.
 
-# 4. Train a linear regression model on the training data.
-model = lm.LinearRegression()
-model.fit(x_train, y_train)
+    Returns:
+        None
+    """
+    # Set the figure size
+    plt.figure(figsize=(8, 6))
 
-# Print the output
-print(f'y = {np.round(model.coef_[3])} * x^4 + {np.round(model.coef_[2])} * x^3 + {np.round(model.coef_[1])} * x^2 + {np.round(model.coef_[0])} * x + {np.round(model.intercept_)}')
+    # Create a heatmap using Seaborn
+    sns.heatmap(cm, annot=True, cmap='Blues', fmt='g', cbar=False)
 
-# 5. Print the coefficient of the highest order term in the polynomial fit.
-# This value should be close to zero, indicating that the neural network has learned
-# the underlying function without overfitting.
-print("Coefficient of x^4 is ", model.coef_[3])
+    # Add labels to the x and y axes
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
 
-# 6. Calculate the mean squared error of the model on the test set.
-# This value should be close to zero, indicating that the neural network has learned
-# the underlying function accurately.
-mse = np.mean((model.predict(x_test) - y_test)**2)
-print("Mean squared error:", mse)
+    # Show the plot
+    plt.show()
+
+
+# Define class labels
+class_names = ['Class 0', 'Class 1']
+
+# Load data
+data = load_breast_cancer()
+X = np.array(pd.DataFrame(data.data, columns=data.feature_names))
+Y = np.array(pd.Series(data.target))
+
+# Classify
+y1 = Y * 0
+y1[np.argwhere(Y == 1)] = 1
+y2 = Y * 0
+y2[np.argwhere(Y == 0)] = 1
+y_new = np.array([y1, y2]).T
+
+state = np.array([0, 1])
+
+# Preprocess data
+x_train, x_test, y_train, y_test = train_test_split(X, y_new, test_size=0.2, random_state=1)
+
+# Define model architecture
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=32, activation='sigmoid'),
+    tf.keras.layers.Dense(units=2, activation='softmax')
+])
+
+# Compile model
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+
+# Train model
+history = model.fit(x_train, y_train, epochs=1000, verbose=0)
+
+# Generate predictions
+y_pred = np.round(model.predict(x_test)).astype(int)
+
+# Evaluate model on validation set
+cm = confusion_matrix(np.dot(y_test, state), np.dot(y_pred, state))
+print('Original Confusion Matrix:\n ', confusion_matrix(np.dot(y_test, state), np.dot(y_test, state)))
+print('Model Confusion Matrix:\n', cm)
+
+# Loss
+print(f'Loss: {history.history["loss"][-1]}')
+loss = history.history['loss']
+
+# MSE
+print('MSE:', mse(y_test, y_pred))
+
+# Plot
+plt.plot(loss)
+plot_confusion_matrix(cm)
